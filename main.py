161303b93fa8d485c19cf14425c8f67b902bcabf
3kh0 <58097612+3kh0@users.noreply.github.com>
2021-09-24 23:50:49 -0400

    messing around with python

---
+++ main.py
@@ -0,0 +1,34 @@
+# My first time using python.
+#
+# Thank you https://www.w3schools.com
+# I learned from there, very helpful stuff.
+# 
+
+if 5 > 2:
+  print("Five is greater than two btw!")
+
+x = 5
+y = "This is y"
+z = 69420
+
+print(x)
+print(y)
+print(z)
+
+a = str(3)
+b = int(3)
+c = float(3)
+
+print(a)
+print(b)
+print(c)
+
+e = "Python is "
+f = "awesome!"
+g =  e + f
+print(g)
+
+i = 1
+while i < 10:
+  print(i)
+  i += 1
